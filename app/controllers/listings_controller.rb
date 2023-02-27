c3a67453c9db74fe56b128d0febf94fe07f3b038
Tim Cai <tim.j.cai@gmail.com>
2023-02-27 17:33:12 +1100

    completed 7 CRUD actions for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -20,6 +20,22 @@ class ListingsController < ApplicationController
     @listing = Listing.find(params[:id])
   end
 
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update(listing_params)
+    redirect_to listing_path(@listing)
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listings_path
+  end
+
   private
 
   def listing_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   get 'listings', to: 'listings#index'
   get 'listings/new', to: 'listings#new'
   post 'listings', to: 'listings#create'
-  get 'listings/:id', to: 'listings#show'
-  get 'listings/:id/edit', to: 'listings#edit'
+  get 'listings/:id', to: 'listings#show', as: 'listing'
+  get 'listings/:id/edit', to: 'listings#edit', as: 'listing_edit'
   patch 'listings/:id', to: 'listings#update'
   delete 'listings/:id', to: 'listings#destroy'
 end
