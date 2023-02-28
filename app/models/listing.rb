dd2657c5a4c67a9180375a384cefbc3f93d3cc43
Tim Cai <tim.j.cai@gmail.com>
2023-02-28 14:28:32 +1100

    merge current progress of bookings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,5 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  has_many :user, through: :booking
   has_many :booking, dependent: :destroy
   has_many :availability, dependent: :destroy
   has_one_attached :photo
